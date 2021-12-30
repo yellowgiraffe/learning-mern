1b736304b9cf85d945b730e0c85c04f9ce0f3647
Olga Mirocz <ol.miroch@gmail.com>
2021-12-30 23:14:23 +0300

    add react components

---
+++ react-goal-list/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './App.css';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
+const App = () => {
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the course'},
+    {id: 'cg2', text: 'Learn all about the course main topic'},
+    {id: 'cg3', text: 'Help other students in the course Q&A section'},
+  ];
+
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal);
+  };
+
+  return <div className="course-goals">
+    <h2>Course Goals</h2>
+    <NewGoal onAddGoal={addNewGoalHandler} />
+    <GoalList goals={courseGoals} />
+  </div>;
+};
+
+export default App;
