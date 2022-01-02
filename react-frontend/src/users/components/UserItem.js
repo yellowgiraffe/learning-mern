5f8d3802c366ae79be29858ceb72d25560c3062d
Olga Mirocz <ol.miroch@gmail.com>
2022-01-02 18:15:02 +0300

    add UserItem component

--- react-frontend/src/users/components/UserItem.js
+++ react-frontend/src/users/components/UserItem.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-import './UserItem.css'
+import './UserItem.css';
 
-const UserItem = (props) => {};
+const UserItem = (props) => {
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{props.name}</h2>
+          <h3>{props.amountOfBooks === 1 ? 'Book: ' : 'Books: '} {props.amountOfBooks}</h3>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default UserItem;
