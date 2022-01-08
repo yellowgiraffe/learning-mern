6455df0d892b6c2d082028fd4b1d17ae8fb56be4
olga_mirocz <ol.miroch@gmail.com>
2022-01-08 23:11:11 +0300

    add input title validation

--- react-frontend/src/books/pages/NewBook.js
+++ react-frontend/src/books/pages/NewBook.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/components/utils/validators';
 import './NewBook.css';
 
 const NewBook = () => {
   return (
     <form className="book-form">
-      <Input element="input" type="text" label="Title" validators={[]} onInput={} />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title" />
     </form>
   );
 };
--- react-frontend/src/shared/components/FormElements/Input.js
+++ react-frontend/src/shared/components/FormElements/Input.js
@@ -1,23 +1,73 @@
 import React, { useReducer } from 'react';
 
+import { validate } from '../utils/validators';
 import './Input.css';
 
-const Input = (props) => {
-  useReducer();
-  const [enteredValue, setenteredValue] = useState('');
-  const [isValid, setIsValid] = useState();
-  const changeHandler = (event) => {};
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    case 'FOCUS':
+      return {
+        ...state,
+        isFocused: true,
+      }
+    default:
+      return state;
+  }
+};
 
-  const element = props.element === 'input' ? (
-  <input id={props.id} type={props.type} placeholder={props.placeholder} onChange={changeHandler} />
+const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isFocused: false,
+    isValid: false
+  });
+
+  const changeHandler = (event) => {
+    dispatch({
+      type: 'CHANGE',
+      val: event.target.value, 
+      validators: props.validators,
+    });
+  };
+
+  const focusHandler= () => {
+    dispatch({
+      type: 'FOCUS'
+    })
+  };
+
+  const element = 
+    props.element === 'input' ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={focusHandler}
+        value={inputState.value} />
   ) : (
-  <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} />
+    <textarea
+      id={props.id}
+      rows={props.rows || 3}
+      onChange={changeHandler}
+      onBlur={focusHandler}
+      value={inputState.value} />
   );
 
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${!inputState.isValid && inputState.isFocused &&
+        'form-control--invalid'}`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
+      {!inputState.isValid && inputState.isFocused && <p>{props.errorText}</p>}
     </div>
   );
 };
