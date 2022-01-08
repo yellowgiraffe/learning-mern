d548ffcdb49f3ade66a95e1ee26f7a184ba2b579
olga_mirocz <ol.miroch@gmail.com>
2022-01-08 22:10:59 +0300

    add input component

--- react-frontend/src/books/pages/NewBook.js
+++ react-frontend/src/books/pages/NewBook.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input';
+import './NewBook.css';
+
 const NewBook = () => {
-  return <h2>Add new book here</h2>;
+  return (
+    <form className="book-form">
+      <Input element="input" type="text" label="Title" validators={[]} onInput={} />
+    </form>
+  );
 };
 
 export default NewBook;
